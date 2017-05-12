081d46712846de808e17be5dbff11b310c004b92
ENDOH takanao <djmchl@gmail.com>
2017-05-13 06:10:09 +1200

    add OpenID connect

--- design/design.go
+++ design/design.go
@@ -6,11 +6,21 @@ import (
 )
 
 const (
-	appID = "example"
+	appID            = "example"
+	google_audiences = "CLIENT-ID"
+
+	authorizationURL = "https://accounts.google.com/o/oauth2/auth"
+	google_issuer    = "https://accounts.google.com"
+	google_jwks_uri  = "https://www.googleapis.com/oauth2/v3/certs"
 )
 
 var _ = API(appID, func() {
 	Title(appID)
+})
+
+var JWT = OAuth2Security("google_id_token", func() {
+	ImplicitFlow(authorizationURL)
+	Scope("read", "read")
 })
 
 var _ = Resource("items", func() {
@@ -18,5 +28,11 @@ var _ = Resource("items", func() {
 	Action("get", func() {
 		Routing(GET(""))
 		Response(OK)
+	})
+	Security(JWT, func() {
+		Scope("read")
+		Metadata("swagger:extension:x-google-issuer", google_issuer)
+		Metadata("swagger:extension:x-google-jwks_uri", google_jwks_uri)
+		//Metadata("swagger:extension:x-google-audiences", google_audiences)
 	})
 })
